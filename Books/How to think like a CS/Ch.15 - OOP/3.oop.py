ae33e10396ab3a643dd82cf3783f7d2ab6b16bfb
Kerem Cumhur <keremcumhur@gmail.com>
2023-03-12 14:54:04 +0000

    OOP

---
+++ Books/How to think like a CS/Ch.15 - OOP/3.oop.py
@@ -0,0 +1,38 @@
+# https://www.youtube.com/watch?v=rq8cL2XMM5M&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc&index=3&ab_channel=CoreySchafer
+
+
+class Employee:
+
+  num_of_emps = 0
+  raise_amount = 1.04
+
+  def __init__(self, first, last, pay):
+    self.first = first
+    self.last = last
+    self.email = first + '.' + last + '@email.com'
+    self.pay = pay
+    Employee.num_of_emps += 1
+
+  def fullname(self):
+    return '{} {}'.format(self.first, self.last)
+
+  def apply_raise(self):
+    self.pay = int(self.pay * self.raise_amount)
+    #self.pay = int(self.pay * Employee.raise_amount)
+
+  @classmethod
+  def set_raise_amount(cls, raise_amount):
+    cls.raise_amount = amount
+
+
+print(Employee.num_of_emps)
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'Employee', 60000)
+
+# print(emp_1.pay)
+# print(emp_1.__dict__)
+# print(Employee.__dict__)
+# emp_1.apply_raise()
+# print(emp_1.pay)
+
+print(Employee.num_of_emps)
