3cf4578fac250fc5b7cdaac6ad4d1874a761af39
Kerem Cumhur <keremcumhur@gmail.com>
2023-03-12 12:02:11 +0000

    OOP things added

--- Books/How to think like a CS/Ch.15 - OOP/1.oop-v1.py
+++ Books/How to think like a CS/Ch.15 - OOP/1.oop-v1.py
@@ -1,21 +1,34 @@
+# https://www.youtube.com/watch?v=BJ-VvGyQxho&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc&index=2&ab_channel=CoreySchafer
+
+
 class Employee:
 
-    def __init__(self, first, last, pay):
-        self.first = first
-        self.last = last
-        self.email = first + '.' + last + '@email.com'
-        self.pay = pay
+  num_of_emps = 0
+  raise_amount = 1.04
 
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last)
+  def __init__(self, first, last, pay):
+    self.first = first
+    self.last = last
+    self.email = first + '.' + last + '@email.com'
+    self.pay = pay
+    Employee.num_of_emps += 1
 
-    def apply_raise(self):
-        self.pay = int(self.pay * 1.04)
+  def fullname(self):
+    return '{} {}'.format(self.first, self.last)
 
+  def apply_raise(self):
+    self.pay = int(self.pay * self.raise_amount)
+    #self.pay = int(self.pay * Employee.raise_amount)
+
+
+print(Employee.num_of_emps)
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
 
+# print(emp_1.pay)
+# print(emp_1.__dict__)
+# print(Employee.__dict__)
+# emp_1.apply_raise()
+# print(emp_1.pay)
 
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
+print(Employee.num_of_emps)
